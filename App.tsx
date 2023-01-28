c9b3cb73094642a79b828971248f66a62cc696a0
Arthur Ribeiro <arthurpsribeiro@MacBook-Pro-de-Arthur.local>
2023-01-28 00:53:23 -0300

    add libs and theme file

--- App.tsx
+++ App.tsx
@@ -1,17 +1,26 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './source/styles/theme';
+
+import Routes from './source/routes';
 
 const App = () => {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <StatusBar barStyle={'dark-content'} />
-      <Text>Events App</Text>
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar />
+            <Routes />
+          </SafeAreaView>
+        </NavigationContainer>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 };
 
---
+++ source/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    main: '#913AF3',
+    secondary: '#5AC922',
+
+    background_primary: '#f4f5f6',
+    background_secondary: '#ffffff',
+
+    success: '#12a454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#E83F5B',
+    attention_light: 'rgba(232, 63, 91, 0.6)',
+
+    button_primary: '#F2A513',
+    button_secondary: '#EE5444',
+
+    text: '#969CB2',
+    text_dark: '#000000',
+
+    shape: '#ffffff',
+    shape_dark: '#000000',
+  },
+};
