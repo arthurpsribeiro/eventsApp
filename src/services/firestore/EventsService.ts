b5727bd97eac1d39c140019f6e4829405e572ace
Arthur Ribeiro <arthurpsribeiro@MacBook-Pro-de-Arthur.local>
2023-01-29 13:12:45 -0300

    add events services

---
+++ src/services/firestore/EventsService.ts
@@ -0,0 +1,31 @@
+import { formatEventData } from '../../utils/formatters/eventsFormatter';
+import { eventsCollection } from './Firestore';
+
+import { EventT } from '../../components/Event/EventItem';
+
+const getAllEvents = async (): Promise<EventT[]> => {
+  try {
+    const eventsSnapshot = await eventsCollection.get();
+
+    const data = eventsSnapshot.docs.map(doc => {
+      const formatedEvent = formatEventData({
+        id: doc.id,
+        date: doc.data().date,
+        name: doc.data().name,
+        category: doc.data().category,
+        spot: doc.data().spot,
+        price: doc.data().price,
+      });
+      return formatedEvent;
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const EventsService = {
+  getAllEvents,
+};
---
+++ src/utils/formatters/eventsFormatter.ts
@@ -0,0 +1,38 @@
+import firestore, {
+  FirebaseFirestoreTypes,
+} from '@react-native-firebase/firestore';
+
+import { EventT, EventCategories } from '../../components/Event/EventItem';
+
+type EventSpotT = {
+  name: string;
+  city: string;
+  state: string;
+};
+
+type EventDataT = {
+  id: string;
+  name: string;
+  spot: EventSpotT;
+  date: FirebaseFirestoreTypes.Timestamp;
+  price: string;
+  category: string;
+};
+
+const formatCategory = (category: string): EventCategories => {
+  return category.toUpperCase() === 'BUSINESS'
+    ? EventCategories.BUSINESS
+    : EventCategories.EDUCATION;
+};
+
+const formatDate = (date: FirebaseFirestoreTypes.Timestamp): Date => {
+  return new firestore.Timestamp(date.seconds, date.nanoseconds).toDate();
+};
+
+export const formatEventData = (eventData: EventDataT): EventT => {
+  return {
+    ...eventData,
+    category: formatCategory(eventData.category),
+    date: formatDate(eventData.date),
+  };
+};
